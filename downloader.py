26147fb7b28275b2f218cae15d352f6ece31defd
tech511 <safinulla7@gmail.com>
2026-03-28 12:23:43 +0600

    Add comment to semaphore for clarity

--- downloader.py
+++ downloader.py
@@ -1,15 +1,13 @@
 import asyncio
 from config import DOWNLOAD_DIR
 
-semaphore = asyncio.Semaphore(2)
+semaphore = asyncio.Semaphore(2)  # controls parallel downloads
 
 async def download_file(m, uid, i):
     for _ in range(3):
         try:
             async with semaphore:
-                return await m.download(
-                    file_name=f"{DOWNLOAD_DIR}/{uid}_{i}"
-                )
+                return await m.download(file_name=f"{DOWNLOAD_DIR}/{uid}_{i}")
         except:
             await asyncio.sleep(1)
     raise Exception("Download Failed")
